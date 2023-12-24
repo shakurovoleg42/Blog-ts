1d833d710749329d3914020c8cd83083dd84e8a1
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-12-24 13:43:13 +0500

    +dev|prod server

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -4,11 +4,16 @@ export interface BuildPaths {
     entry: string;
     build: string;
     html: string;
+}
 
+export interface BuildEnv {
+    mode: BuildMode;
+    port: number;
 }
 
 export interface BuildOptions {
     mode: BuildMode;
     paths: BuildPaths;
     isDev: boolean;
+    port: number;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import {someFn} from './test'
 
 someFn(123)
+
+document.body.innerHTML = `<div>HHAHAHAHAHAHA</div>`
